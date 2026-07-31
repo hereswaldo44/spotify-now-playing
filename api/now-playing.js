b6670d7eeef811902e16727e360c91500942000b
hereswaldo44 <tjryan44@gmail.com>
2026-07-31 22:56:50 +0100

    Update now-playing.js

--- api/now-playing.js
+++ api/now-playing.js
@@ -2,27 +2,21 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const token = await getAccessToken();
+
   const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
     headers: { Authorization: `Bearer ${token}` },
   });
 
-  if (response.status === 204 || response.status > 400) {
-    return res.json({ isPlaying: false });
+  if (response.status === 204) {
+    return res.json({ isPlaying: false, debug: 'spotify returned 204 - nothing playing' });
+  }
+
+  if (response.status > 400) {
+    return res.json({ isPlaying: false, debug: `spotify error ${response.status}` });
   }
 
   const song = await response.json();
-
-  if (!song?.item) {
-    return res.json({ isPlaying: false });
-  }
-
-  return res.json({
-    isPlaying: song.is_playing,
-    title: song.item.name,
-    artist: song.item.artists.map(a => a.name).join(', '),
-    albumArt: song.item.album.images[0]?.url,
-    songUrl: song.item.external_urls.spotify,
-  });
+  return res.json({ isPlaying: song.is_playing, raw: song?.item?.name ?? 'no item', song });
 }
 
 async function getAccessToken() {
